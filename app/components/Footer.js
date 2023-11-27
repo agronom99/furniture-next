e88a013b8314a03b3a880db008938f1dd36d4f90
Vitalii MV <agronom9913@gmail.com>
2023-11-27 18:16:08 +0200

    2023-11-27 18-20

---
+++ app/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className=" bg-slate-600 h-52 m-1 rounded-xl p-5 ">Footer</div>;
+};
+
+export default Footer;
--- app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div className=' h-24 bg-slate-600'>Footer</div>
-  )
-}
-
-export default Footer
