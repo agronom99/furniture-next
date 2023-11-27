7d37b6a2acea35eecfdbb8127b54a147b91b5626
Vitalii MV <agronom9913@gmail.com>
2023-11-27 23:11:51 +0200

    2023-11-27 23-11

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import Rectangle from "../../public/Rectangle_1441_h.jpg";
 
 const Footer = () => {
-  return <div className=" bg-slate-600 h-52 m-1 rounded-xl p-5 ">Footer</div>;
+  return (
+    <div className=" rounded-xl">
+      <Image
+        className="absolute z-0 rounded-xl w-full"
+        alt="Rectangle"
+        src={Rectangle}
+        // height={200}
+        priority
+      />
+      <div className="text-white relative text-center p-5">
+      <h3>Footer</h3>
+      <h3>Footer</h3>
+      <h3>Footer</h3>
+      </div>
+
+    </div>
+  );
 };
 
 export default Footer;
