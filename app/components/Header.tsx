4e07f9c58823c6804c29bd64b9b4501eb283c795
Vitalii MV <agronom9913@gmail.com>
2023-11-27 22:46:30 +0200

    2023-11-27 22-45

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,20 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Rectangle from "../../public/Rectangle_1441_h.jpg";
 
 const Header = () => {
   return (
-    <div className=" flex flex-col lg:flex-row justify-around ">
-      <div className="">
+    <div className=" flex flex-col lg:flex-row justify-around relative ">
+      <Image
+        className="absolute z-0 rounded-xl w-full h-full   "
+        alt="Rectangle"
+        src={Rectangle}
+        // height={200}
+        priority
+      />
+      <div className="z-10 ">
         <a
           className=""
           href="/"
@@ -23,8 +31,7 @@ const Header = () => {
           />
         </a>
       </div>
-      <div className="flex flex-col sm:flex-row text-center items-center ">
-        
+      <div className="flex flex-col sm:flex-row text-center items-center z-10 text-white ">
         <Link href="/about_us" className="" rel="noopener noreferrer">
           <h2
             className={`text-sm md:text-xl font-semibold m-1 md:m-3 p-3 border-2 rounded-xl hover:bg-slate-400`}
@@ -41,7 +48,11 @@ const Header = () => {
           </h2>
         </Link>
 
-        <Link href="/delivery_and_payment" className=" " rel="noopener noreferrer">
+        <Link
+          href="/delivery_and_payment"
+          className=" "
+          rel="noopener noreferrer"
+        >
           <h2
             className={`text-sm md:text-xl font-semibold m-1 md:m-3 p-3 border-2 rounded-xl hover:bg-slate-400 active:bg-slate-400`}
           >
