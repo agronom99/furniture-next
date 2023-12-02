5e2f60345229ddfac6e2322af272dae4a8cfaf27
Vitalii MV <agronom9913@gmail.com>
2023-12-02 23:58:35 +0200

    2023.12.02 23-58

--- app/components/FurnitureBlock/index.jsx
+++ app/components/FurnitureBlock/index.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 const FurnitureBlock = ({ title, price, imageURL,article,availability}) => {
  
   return (
-    <div className=" border-neutral-800 border-solid rounded-xl m-2 justify-center items-center flex flex-col">
+    <div className=" border-neutral-800 border-solid rounded-xl m-2 justify-center items-center flex flex-col p-3 h-auto w-48">
      
       <Image 
-      className=" w-auto "
+      className="h-auto w-auto"
       alt="img12" 
       src={imageURL} 
-      height={20}
-      width={30}
+      height={100}
+      width={125}
        priority />
        <div>
        <p>Арт.{article}</p>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains: ['dodopizza.azureedge.net']
+        domains: ['nbuhrmvoogojiuhaykgk.supabase.co']
     }
 }
 
