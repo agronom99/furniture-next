a623ff604ee379a08c664508a04abd372e940982
Vitalii MV <agronom9913@gmail.com>
2023-12-11 15:37:30 +0200

    2023.12.11 15-40

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,5 +1,5 @@
-// "use client";
-// import React, { useEffect } from "react";
+"use client";
+import React, { useEffect } from "react";
 import Categories from "../components/Categories";
 
 const AboutUs = () => {
