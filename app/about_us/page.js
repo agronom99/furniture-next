d4ce1017b18e792c3e7c3704ac0a2d77ce7ca732
Vitalii MV <agronom9913@gmail.com>
2023-12-11 15:33:47 +0200

    2023.12.10  15-33

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -5,10 +5,10 @@ import Categories from "../components/Categories";
 const AboutUs = () => {
   const [categoryId, setCategoryId] = React.useState(0);
 
-  useEffect(() => {
-    // Встановлюємо позицію прокрутки на верх сторінки при завантаженні компонента
-    window.scrollTo(0, 0);
-  }, []); // Порожній масив вказує, що ефект викликається тільки при монтажі та демонтажі компонента
+  // useEffect(() => {
+  //   // Встановлюємо позицію прокрутки на верх сторінки при завантаженні компонента
+  //   window.scrollTo(0, 0);
+  // }, []); // Порожній масив вказує, що ефект викликається тільки при монтажі та демонтажі компонента
 
   return (
     <div className=" h-auto w-11/12 mx-auto border-neutral-800 border-solid rounded-xl ">
